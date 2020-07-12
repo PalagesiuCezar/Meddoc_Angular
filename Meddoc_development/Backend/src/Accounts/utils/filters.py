a69566684ff70eb2630f1297b0347110b6a75396
Palagesiu Cezar Ioan <67911458+PalagesiuCezar@users.noreply.github.com>
2020-07-12 15:42:34 +0300

    Create filters.py

---
+++ Meddoc_development/Backend/src/Accounts/utils/filters.py
@@ -0,0 +1,16 @@
+import rest_framework_filters as filters
+
+from Accounts.models import User
+
+
+class UserFilter(filters.FilterSet):
+
+    class Meta:
+        model = User
+        fields = {
+            'first_name': '__all__',
+            'last_name': '__all__',
+            'email': '__all__',
+            'is_pacient': '__all__',
+            'is_doctor': '__all__'
+        }
